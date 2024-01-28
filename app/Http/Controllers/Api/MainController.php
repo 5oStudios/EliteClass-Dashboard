5c4cce3687da939d10c3f28a952288e8007cc749
ElGamal <ahmedelgamal5320@gmail.com>
2024-01-28 16:34:42 +0200

    check:if course free in adding to card 2

--- app/Http/Controllers/Api/MainController.php
+++ app/Http/Controllers/Api/MainController.php
@@ -1849,12 +1849,10 @@ class MainController extends Controller
                 $total_amount = $total_amount + $c->offer_price;
             } else {
 
-                if (is_null($c->offer_type) && $c->offer_price) {
-                    if ($c->offer_price == 0) {
-                        $total_amount += $c->price;
-                    } else {
-                        $total_amount += $c->offer_price;
-                    }
+                if (is_null($c->offer_type) && $c->offer_price == 0) {
+                    $total_amount += $c->price;
+                } elseif (is_null($c->offer_type) && $c->offer_price) {
+                    $total_amount += $c->offer_price;
                 } else {
                     //fixed
                     if ($c->offer_type == 'fixed') {
@@ -2071,11 +2069,11 @@ class MainController extends Controller
         $resp = [
             'course' => $courseDetail,
             'instructor' => [
-                'id' => $course->teacher->id,
-                'name' => $course->teacher->fname . ' ' . $course->teacher->lname,
-                'image' => url('/images/user_img/' . $course->teacher->user_img),
-                'short_info' => $course->teacher->short_info
-            ],
+                    'id' => $course->teacher->id,
+                    'name' => $course->teacher->fname . ' ' . $course->teacher->lname,
+                    'image' => url('/images/user_img/' . $course->teacher->user_img),
+                    'short_info' => $course->teacher->short_info
+                ],
             'whatlearns' => $whatlearns,
             'reviews' => $reviewszz ?? null,
             'reviews_added' => $reviews_by_user,
@@ -2387,42 +2385,42 @@ class MainController extends Controller
 
         $course = Course::where('status', 1)
             ->with([
-                'include' => function ($query) {
-                    $query->where('status', 1);
-                }
-            ])
+                    'include' => function ($query) {
+                        $query->where('status', 1);
+                    }
+                ])
             ->with([
-                'whatlearns' => function ($query) {
-                    $query->where('status', 1);
-                }
-            ])
+                    'whatlearns' => function ($query) {
+                        $query->where('status', 1);
+                    }
+                ])
             ->with([
-                'related' => function ($query) {
-                    $query->where('status', 1);
-                }
-            ])
+                    'related' => function ($query) {
+                        $query->where('status', 1);
+                    }
+                ])
             ->with('review')
             ->with([
-                'language' => function ($query) {
-                    $query->where('status', 1);
-                }
-            ])
+                    'language' => function ($query) {
+                        $query->where('status', 1);
+                    }
+                ])
             ->with('user')
             ->with([
-                'order' => function ($query) {
-                    $query->where('status', 1);
-                }
-            ])
+                    'order' => function ($query) {
+                        $query->where('status', 1);
+                    }
+                ])
             ->with([
-                'chapter' => function ($query) {
-                    $query->where('status', 1);
-                }
-            ])
+                    'chapter' => function ($query) {
+                        $query->where('status', 1);
+                    }
+                ])
             ->with([
-                'courseclass' => function ($query) {
-                    $query->where('status', 1);
-                }
-            ])
+                    'courseclass' => function ($query) {
+                        $query->where('status', 1);
+                    }
+                ])
             ->with('policy')->get();
 
         return response()->json(array('course' => $course), 200);
@@ -2610,11 +2608,11 @@ class MainController extends Controller
 
             CourseProgress::where('course_id', $course->id)->where('user_id', '=', $auth->id)
                 ->update([
-                    'progress' => $progres,
-                    'mark_chapter_id' => $course_return,
-                    'all_chapter_id' => $chapters->pluck('id'),
-                    'updated_at' => \Carbon\Carbon::now()->toDateTimeString(),
-                ]);
+                        'progress' => $progres,
+                        'mark_chapter_id' => $course_return,
+                        'all_chapter_id' => $chapters->pluck('id'),
+                        'updated_at' => \Carbon\Carbon::now()->toDateTimeString(),
+                    ]);
 
             return response()->json('Updated sucessfully !', 200);
 
@@ -3892,9 +3890,9 @@ class MainController extends Controller
 
                 ReviewHelpful::where('id', $help->id)
                     ->update([
-                        'review_like' => '1',
-                        'review_dislike' => '0',
-                    ]);
+                            'review_like' => '1',
+                            'review_dislike' => '0',
+                        ]);
             } else {
 
                 $created_review = ReviewHelpful::create(
@@ -3909,8 +3907,8 @@ class MainController extends Controller
 
                 ReviewHelpful::where('id', $created_review->id)
                     ->update([
-                        'review_dislike' => '0',
-                    ]);
+                            'review_dislike' => '0',
+                        ]);
             }
         } elseif ($request->review_dislike == '1') {
 
@@ -3918,9 +3916,9 @@ class MainController extends Controller
 
                 ReviewHelpful::where('id', $help->id)
                     ->update([
-                        'review_dislike' => '1',
-                        'review_like' => '0',
-                    ]);
+                            'review_dislike' => '1',
+                            'review_like' => '0',
+                        ]);
             } else {
 
                 $created_review = ReviewHelpful::create(
@@ -3935,19 +3933,19 @@ class MainController extends Controller
 
                 ReviewHelpful::where('id', $created_review->id)
                     ->update([
-                        'review_like' => '0',
-                    ]);
+                            'review_like' => '0',
+                        ]);
             }
         } elseif ($help->review_like == '1') {
             ReviewHelpful::where('id', $help->id)
                 ->update([
-                    'review_like' => '0',
-                ]);
+                        'review_like' => '0',
+                    ]);
         } elseif ($help->review_dislike == '1') {
             ReviewHelpful::where('id', $help->id)
                 ->update([
-                    'review_dislike' => '0',
-                ]);
+                        'review_dislike' => '0',
+                    ]);
         }
 
         return response()->json(array('message' => 'Updated Successfully', 'status' => 'success'), 200);
@@ -3981,10 +3979,10 @@ class MainController extends Controller
                     'imagepath' => url('images/course/' . $course->preview_image),
                     'in_wishlist' => Is_wishlist::in_wishlist($course->id),
                     'instructor' => array(
-                        'id' => $course->user->id,
-                        'name' => $course->user->fname . ' ' . $course->user->lname,
-                        'image' => url('/images/user_img/' . $course->user->user_img),
-                    ),
+                            'id' => $course->user->id,
+                            'name' => $course->user->fname . ' ' . $course->user->lname,
+                            'image' => url('/images/user_img/' . $course->user->user_img),
+                        ),
                 );
             }
 
@@ -4026,10 +4024,10 @@ class MainController extends Controller
                 $order = Order::where('id', $orderInstallemts[$i]['order_id'])
                     ->with('courses', 'bundle')
                     ->with([
-                        'payment_plan' => function ($query) {
-                            $query->where('due_date', '<=', now()->addDays(2))->where('status', null);
-                        }
-                    ])
+                            'payment_plan' => function ($query) {
+                                $query->where('due_date', '<=', now()->addDays(2))->where('status', null);
+                            }
+                        ])
                     ->get()->toArray();
 
                 // dd($order);

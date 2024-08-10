b8a90094bb61888babe2c1f7c4fcd4ce5a4af4fa
Ahmed Agha <85362899+aagha86@users.noreply.github.com>
2024-08-10 16:29:52 +0300

    Create CreateAdminUser.php

---
+++ app/Console/Commands/CreateAdminUser.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class CreateAdminUser extends Command
+{
+    protected $signature = 'make:admin {email} {name} {password}';
+    protected $description = 'Create a new user with admin role';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        $email = $this->argument('email');
+        $name = $this->argument('name');
+        $password = $this->argument('password');
+
+        // Create the user
+        $user = User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => Hash::make($password),
+        ]);
+
+        // Assign admin role
+        $user->assignRole('admin');
+
+        $this->info('Admin user created successfully.');
+    }
+}

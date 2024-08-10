d3f6a0dd32500f5fedac73d9d54f268534e6be11
Ahmed Agha <85362899+aagha86@users.noreply.github.com>
2024-08-10 16:33:23 +0300

    Update CreateAdminUser.php

--- app/Console/Commands/CreateAdminUser.php
+++ app/Console/Commands/CreateAdminUser.php
@@ -3,7 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use App\Models\User;
+use App\User;
 use Illuminate\Support\Facades\Hash;
 
 class CreateAdminUser extends Command
